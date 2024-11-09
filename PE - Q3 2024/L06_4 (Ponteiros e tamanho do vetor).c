629b3f2648ca0865a6753c479758182c8cde8956
Leo <Leonardoginmesquita@gmail.com>
2024-11-09 14:31:25 -0300

    Create L06_4 (Ponteiros e tamanho do vetor).c

---
+++ PE - Q3 2024/L06_4 (Ponteiros e tamanho do vetor).c
@@ -0,0 +1,2 @@
+/*Verificar o tamanho de um vetor a partir da diferença entre o endereço do primeiro e do último elemento. Compare com o resultado obtido pela função sizeof. Para este exemplo, considere um vetor de 10 elementos.*/
+
