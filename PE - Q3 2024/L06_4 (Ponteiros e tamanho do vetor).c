b1e3aefc2327d6f4f39f9e7c23eb726e30b503a5
Leo <Leonardoginmesquita@gmail.com>
2024-11-10 02:57:32 -0300

    Update L06_4 (Ponteiros e tamanho do vetor).c

--- PE - Q3 2024/L06_4 (Ponteiros e tamanho do vetor).c
+++ PE - Q3 2024/L06_4 (Ponteiros e tamanho do vetor).c
@@ -1,2 +1,11 @@
 /*Verificar o tamanho de um vetor a partir da diferença entre o endereço do primeiro e do último elemento. Compare com o resultado obtido pela função sizeof. Para este exemplo, considere um vetor de 10 elementos.*/
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int vetor[10];
+    printf("O número de elementos usando ponteiros é %d.\n",*(&vetor+1)-*(&vetor));
+    printf("O número de elementos usando sizeof é %d.\n",sizeof(vetor)/sizeof(int));
+    return 0;
+}
